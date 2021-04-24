37c54e002441e4da5bb8f5b2b9b700ec651602b8
unknown <simon.atias.abregu@gmail.com>
2021-04-24 06:09:54 -0300

    Terminadas todas las requests (sin estructura de clases)

--- TP2/main.ts
+++ TP2/main.ts
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 var mysql = require('mysql');
 var connection = mysql.createConnection({
@@ -8,32 +8,140 @@ var connection = mysql.createConnection({
     user: 'root',
     password : '1234',
     database :  'ecommerce'
-})
+});
 
-connection.connect();
+app.get('/productos', (req, res) => {
+  
+    var mi_query = new String("SELECT * FROM productos WHERE 1 ");
 
-/*app.get('/procuctos', (req, res) => {
-    
-    connection.query( 'SELECT * FROM ecommerce.productos WHERE nombre like "%PS2%";', function (error, results, field){
-        if (error) throw error;
+    if(req.query.busqueda!=null){
+        mi_query = mi_query.concat(("AND nombre like '%"+req.query.busqueda+"%' ").toString());
+    }
+    if(req.query.usado!=null){
+        mi_query = mi_query.concat(("AND usado = "+req.query.usado+" ").toString());
+    }
+    if(req.query.orden!=null){
+        mi_query = mi_query.concat(("order by "+req.query.orden+";").toString());
+    }
+
+    connection.query(mi_query, function (error, results, field) {
+        if (error)
+            throw error;
         return res.json(results);
     });
 
-})*/
+});
 
-app.get('/users/:userId/books/:bookId', function(req, res){
+app.get('/usuarios/:id_usuario/fav', (req, res) => {
+    
+    connection.query("SELECT productos.id, productos.vendedor, productos.nombre, productos.precio, productos.stock, productos.usado FROM ecommerce.favoritos INNER JOIN usuarios ON usuarios.id=favoritos.id_usuario INNER JOIN productos ON favoritos.id_producto=productos.id WHERE usuarios.id = "+req.params.id_usuario+";", function (error, results, field) {
+        if (error)
+            throw error;
+        return res.json(results);
+    });
 
-    res.send(req.params);
+});
+
+app.post('/usuarios/:id_usuario/fav', (req, res) =>{
+
+    connection.query("insert into favoritos values (null, "+req.params.id_usuario+", "+req.query.id_producto+");", function (error, results, field){
+        if(error){
+            throw error;
+        }
+        else{
+            res.send('InsertadE');
+        }
+    })
+        
+
+})
+
+app.delete('/usuarios/:id_usuario/fav', (req, res) =>{
+
+    connection.query("DELETE FROM favoritos WHERE id_usuario = "+req.params.id_usuario+" AND id_producto = "+req.query.id_producto+";", function (error, results, field){
+        if(error){
+            throw error;
+        }
+        else{
+            res.send('DeleteadE');
+        }
+    })
+
+})
+
+app.get('/usuarios/:id_usuario/compras', (req, res) =>{
+
+    connection.query("SELECT * FROM compras WHERE id_usuario = "+req.params.id_usuario+";", function (error, results, field){
+        if(error){
+            throw error;
+        }
+        else{
+            return res.json(results);
+        }
+    })
+
+})
+
+app.post('/usuarios/:id_usuario/compras', (req, res) =>{
+
+    connection.query("Insert into compras values (null, "+req.params.id_usuario+", "+req.query.id_producto+", "+req.query.cantidad+", NOW(), 0, 0);", function (error, results, field){
+        if(error){
+            throw error;
+        }
+        else{
+            res.send('InsertadE');
+        }
+    })
+        
+
+})
+
+app.get('/usuarios/:id_usuario/calificaciones', (req, res) =>{
+
+    connection.query("SELECT id_comprador , id_vendedor AS 'vendedor', calificacion, fecha, 'compra' AS tipo_intercambio FROM calificaciones_compradores WHERE id_comprador = "+req.params.id_usuario+" UNION SELECT id_vendedor AS mi_id, id_comprador AS 'cliente/vendedo', calificacion, fecha, 'venta' AS tipo_intercambio FROM calificaciones_vendedores WHERE id_comprador = "+req.params.id_usuario+";", function (error, results, field){
+        if(error){
+            throw error;
+        }
+        else{
+            return res.json(results);
+        }
+    })
+
+})
+
+app.post('/usuarios/:id_usuario/calificaciones', (req, res) =>{
+
+    var id_operacion = req.query.id_operacion;
+
+    connection.query('SELECT id_usuario AS id_comprador, vendedor FROM compras INNER JOIN productos ON id_producto=productos.id WHERE compras.id = '+id_operacion+';', function (error, resultsQuery1, fields) {
+        if (error) throw error;
+        if(resultsQuery1[0].id_comprador==req.params.id_usuario){
+            connection.query("Insert into calificaciones_compradores values (null, "+req.params.id_usuario+","+resultsQuery1[0].vendedor+","+req.query.calificacion+", NOW());", function (error, results, field){
+                if(error){
+                    throw error;
+                }
+                else{
+                    res.send('InsertadE');
+                }
+            })
+        }
+        else {
+            connection.query("Insert into calificaciones_vendedores values (null, "+req.params.id_usuario+","+resultsQuery1[0].id_comprador+","+req.query.calificacion+", NOW());", function (error, results, field){
+                if(error){
+                    throw error;
+                }
+                else{
+                    res.send('InsertadE');
+                }
+            })
+        }
+    });
+
+    
+        
 
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
-
-
-
-
-
-
-
+});
---
+++ TP2/pruea.js
@@ -0,0 +1,6 @@
+var str0 = new String("asdasd");
+var str1 = new String("jkljkljkl");
+var str0 = str0.concat(str1);
+console.log(str0);
+
+console.log(str0.replace('oma',"reemplazado"));
