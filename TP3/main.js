0431fbe2cd3bcab6ec4d7ad4dbb775b3af5f36f7
Magali <alumno21.cristobo.magali@ipm.edu.ar>
2021-05-17 11:45:05 -0300

    pruebas fork instix

--- TP3/main.js
+++ TP3/main.js
@@ -1,9 +1,37 @@
-var express = require('express');
-var app = express();
-var port = 3000;
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+var cluster = require('cluster');
+var process = require('process');
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "1234",
+    database: "ecommerce"
 });
-app.listen(port, function () {
-    console.log("Example app listening at http://localhost:" + port);
-});
+if (cluster.isWorker) {
+    process.on('message', function (msg) {
+        if (msg == 'hola, negry') {
+            console.log(msg);
+            process.kill(process.pid);
+        }
+        else if (msg == 'hola, pa') {
+            console.log(msg);
+            process.kill(process.pid);
+        }
+    });
+}
+else {
+    var express = require('express');
+    var app = express();
+    var port_1 = 3000;
+    app.get('/negry', function (req, res) {
+        var worker = cluster.fork();
+        worker.send('hola, negry');
+    });
+    app.get('/pa', function (req, res) {
+        var worker = cluster.fork();
+        worker.send('hola, pa');
+    });
+    app.listen(port_1, function () {
+        console.log("Example app listening at http://localhost:" + port_1);
+    });
+}
--- TP3/main.ts
+++ TP3/main.ts
@@ -1,7 +1,5 @@
-
-const express = require('express')
-const app = express()
-const port = 3000
+const cluster = require('cluster');
+const process = require('process');
 
 var mysql = require('mysql');
 var connection = mysql.createConnection({
@@ -11,12 +9,42 @@ var connection = mysql.createConnection({
     database:"ecommerce"
 });
 
-app.get('/funciones', (req, res) => {
+if(cluster.isWorker){
 
+    process.on('message', (msg) => {
+        if(msg=='hola, negry'){
+            console.log(msg);
+            process.kill(process.pid);
+        }
+        else if(msg=='hola, pa'){
+            console.log(msg);
+            process.kill(process.pid);
+        }
+    })
+
+}
+else{
     
+    const express = require('express');
+    const app = express();
+    const port = 3000;
+    
+    app.get('/negry', (req, res) => {
+        const worker = cluster.fork();
+        worker.send('hola, negry');
+    })
 
-})
+    app.get('/pa', (req, res) => {
+        const worker = cluster.fork();
+        worker.send('hola, pa');
+    })
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+    app.listen(port, () => {
+        console.log(`Example app listening at http://localhost:${port}`)
+    });
+
+}
+
+
+
+
